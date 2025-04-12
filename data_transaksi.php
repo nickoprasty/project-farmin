0e7ce057aa49adfb34ae3efea161fbd361d0f819
nickoprasty <nickoprasetya17@gmail.com>
2025-04-12 17:30:37 +0700

    update chart

--- data_transaksi.php
+++ data_transaksi.php
@@ -1,10 +1,16 @@
 <?php 
 include 'database.php';
 
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');
+header('Content-Type: application/json');
+
 $sql = "
-    SELECT p.nama_pupuk, t.jumlah 
+    SELECT p.nama_pupuk, SUM(t.jumlah) as jumlah 
     FROM transaksi_pupuk t
     JOIN pupuk p ON t.id_pupuk = p.id_pupuk
+    GROUP BY p.nama_pupuk
 ";
 
 $res = $db->query($sql);
@@ -19,6 +25,5 @@ while($row = $res->fetch_assoc()){
     $data[] = $row;
 }
 
-header('Content-Type: application/json');
 echo json_encode($data);
 ?>
